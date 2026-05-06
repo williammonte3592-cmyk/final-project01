da4ec8640c9b982ced579634d72b15d3f7e0ba7e
monte <williammonte3592@gmail.com>
2026-05-06 13:42:34 +0300

    Update comment form textarea to reduce height and improve user experience

---
+++ script.js
@@ -0,0 +1,14 @@
+const commentform = document.querySelector('.comment-form');
+
+commentform.addEventListener('submit', function (e)) {
+e.preventdefault();
+
+const nameinput = this.querySelector('input[placeholder="Enter Name"]');
+const emailinput = this.querySelector('input[placeholder="Enter email"]')
+const commentArea = this.querySelector('textarea');
+
+
+
+
+
+}
