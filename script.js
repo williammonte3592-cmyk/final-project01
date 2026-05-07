096cf57a78fe2828effea7084ad57160e65496f6
monte <williammonte3592@gmail.com>
2026-05-07 16:16:27 +0300

    Enhance comment form functionality by saving user input to local storage

--- script.js
+++ script.js
@@ -5,9 +5,16 @@ document.addEventListener("DOMContentLoaded", () => {
     commentForm.addEventListener("submit", function (event) {
       event.preventDefault();
 
+
       const name = document.getElementById("name").value.trim();
       const email = document.getElementById("email").value.trim();
       const comment = document.querySelector("textarea").value.trim();
+
+        localStorage.setItem("name", name)
+        localStorage.setItem("email", email)
+        localStorage.setItem("comment",comment)
+
+
 
       if (name === "" || email === "" || comment === "") {
         alert("Please fill in all fields before posting.");
